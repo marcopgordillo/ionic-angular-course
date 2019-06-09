a3849222fb0fee5e4d1d0fcdc15e4b6fa48fd5ad
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-06-09 02:53:50 -0400

    Switching between auth modes

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -12,6 +12,7 @@ import {NgForm} from '@angular/forms';
 export class AuthPage implements OnInit {
 
   isLoading = false;
+  isLogin = true;
 
   constructor(private authService: AuthService,
               private router: Router,
@@ -35,5 +36,9 @@ export class AuthPage implements OnInit {
 
   onSubmit(form: NgForm) {
     console.log(form);
+  }
+
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
   }
 }
