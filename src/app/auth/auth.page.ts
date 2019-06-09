8201a96ebd94a487d86e722558e8e9178cca75fd
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-06-09 02:35:31 -0400

    Adding a template-driven form

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AuthService} from './auth.service';
 import {Router} from '@angular/router';
 import {LoadingController} from '@ionic/angular';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -30,5 +31,9 @@ export class AuthPage implements OnInit {
         this.router.navigateByUrl('/places/tabs/discover');
       }, 1500);
     });
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
