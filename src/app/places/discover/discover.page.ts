9b0109e4497e716a2a64ed7ff780ba3ee4d5e1c7
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-06-13 08:47:58 -0400

    UI State with Bookable Places

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -4,6 +4,7 @@ import {Place} from '../place.model';
 import {MenuController} from '@ionic/angular';
 import {SegmentChangeEventDetail} from '@ionic/core';
 import {Subscription} from 'rxjs';
+import {AuthService} from '../../auth/auth.service';
 
 @Component({
   selector: 'app-discover',
@@ -14,15 +15,18 @@ export class DiscoverPage implements OnInit, OnDestroy {
 
   loadedPlaces: Place[];
   listedLoadedPlaces: Place[];
+  relevantPlaces: Place[];
   private placesSub: Subscription;
 
   constructor(private placesService: PlacesService,
-              private menuCtrl: MenuController) { }
+              private menuCtrl: MenuController,
+              private authService: AuthService) { }
 
   ngOnInit() {
     this.placesSub = this.placesService.places.subscribe(places => {
       this.loadedPlaces = places;
-      this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+      this.relevantPlaces = this.loadedPlaces;
+      this.listedLoadedPlaces = this.relevantPlaces.slice(1);
     });
   }
 
@@ -31,7 +35,12 @@ export class DiscoverPage implements OnInit, OnDestroy {
   }
 
   onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
-    console.log(event.detail);
+    if(event.detail.value === 'all') {
+      this.relevantPlaces = this.loadedPlaces;
+    } else {
+      this.relevantPlaces = this.loadedPlaces.filter(place => place.userId !== this.authService.userId);
+    }
+    this.listedLoadedPlaces = this.relevantPlaces.slice(1);
   }
 
   ngOnDestroy(): void {
