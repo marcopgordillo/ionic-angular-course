67c51510a88760918e3c5e211c6bd0890045455c
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-06-18 12:13:50 -0400

    Testing the Location feature

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -74,25 +74,12 @@ export class LocationPickerComponent implements OnInit {
               return;
             }
 
-            const pickedLocation: PlaceLocation = {
+            const coordinates: Coordinates = {
               lat: modalData.data.lat,
-              lng: modalData.data.lng,
-              address: null,
-              staticMapImageUrl: null
+              lng: modalData.data.lng
             };
-            this.isLoading = true;
-            this.getAddress(modalData.data.lat, modalData.data.lng)
-                .pipe(
-                    switchMap(address => {
-                      pickedLocation.address = address;
-                      return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
-                    })
-                ).subscribe(staticMapImageUrl => {
-              pickedLocation.staticMapImageUrl = staticMapImageUrl;
-              this.selectedLocationImage = staticMapImageUrl;
-              this.isLoading = false;
-              this.locationPick.emit(pickedLocation);
-            });
+
+            this.createPlace(coordinates.lat, coordinates.lng);
           });
           modalEl.present();
         });
@@ -104,10 +91,17 @@ export class LocationPickerComponent implements OnInit {
       return;
     }
 
+    this.isLoading = true;
     Plugins.Geolocation
         .getCurrentPosition()
         .then(geoPosition => {
-          const coordinates: Coordinates = {lat: geoPosition.coords.latitude, lng: geoPosition.coords.longitude};
+          const coordinates: Coordinates = {
+            lat: geoPosition.coords.latitude,
+            lng: geoPosition.coords.longitude
+          };
+
+          this.createPlace(coordinates.lat, coordinates.lng);
+          this.isLoading = false;
         })
         .catch(err => this.showErrorAlert());
   }
@@ -116,8 +110,32 @@ export class LocationPickerComponent implements OnInit {
     this.alertCtrl.create(
         {
           header: 'Could not fetch location',
-          message: 'Please use the map to pick a location!'
+          message: 'Please use the map to pick a location!',
+          buttons: ['Okay']
         }
     ).then(alertEl => alertEl.present());
+  }
+
+  private createPlace(lat: number, lng: number) {
+    const pickedLocation: PlaceLocation = {
+      lat: lat,
+      lng: lng,
+      address: null,
+      staticMapImageUrl: null
+    };
+
+    this.isLoading = true;
+    this.getAddress(lat, lng)
+        .pipe(
+            switchMap(address => {
+              pickedLocation.address = address;
+              return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
+            })
+        ).subscribe(staticMapImageUrl => {
+      pickedLocation.staticMapImageUrl = staticMapImageUrl;
+      this.selectedLocationImage = staticMapImageUrl;
+      this.isLoading = false;
+      this.locationPick.emit(pickedLocation);
+    });
   }
 }
