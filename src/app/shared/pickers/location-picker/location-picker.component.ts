50d6f3cc2d2ae754fb5ada0bc5395f1a1ef73626
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-06-18 11:49:25 -0400

    Getting the user location

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,11 +1,12 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {ModalController} from '@ionic/angular';
+import {ActionSheetController, AlertController, ModalController} from '@ionic/angular';
 import {MapModalComponent} from '../../map-modal/map-modal.component';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../../environments/environment';
 import {map, switchMap} from 'rxjs/operators';
-import {PlaceLocation} from '../../../places/location.model';
+import {Coordinates, PlaceLocation} from '../../../places/location.model';
 import {of} from 'rxjs';
+import {Capacitor, Plugins} from '@capacitor/core';
 
 @Component({
   selector: 'app-location-picker',
@@ -22,11 +23,49 @@ export class LocationPickerComponent implements OnInit {
   locationPick = new EventEmitter<PlaceLocation>();
 
   constructor(private modalCtrl: ModalController,
-              private http: HttpClient) { }
+              private http: HttpClient,
+              private actionSheetCtrl: ActionSheetController,
+              private alertCtrl: AlertController) { }
 
   ngOnInit() {}
 
   onPickLocation() {
+    this.actionSheetCtrl.create(
+        {
+          header: 'Please Choose',
+          buttons: [
+            {text: 'Auto-Locate', handler: () => {
+              this.locateUser();
+              }},
+            {text: 'Pick on Map', handler: () => {
+              this.openMap();
+              }},
+            {text: 'Cancel', role: 'cancel'},
+          ]
+        }
+        ).then(actionSheetEl => {
+          actionSheetEl.present();
+    });
+  }
+
+  private getAddress(lat: number, lng: number) {
+    return this.http
+        .get<any>(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${this.GOOGLE_API_KEY}`)
+        .pipe(
+            map((geoData: any) => {
+              if (!geoData || !geoData.results || geoData.results.length === 0) {
+                return null;
+              }
+              return geoData.results[0].formatted_address;
+            })
+        );
+  }
+
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap&markers=color:red%7Clabel:Place%7C${lat},${lng}&key=${this.GOOGLE_API_KEY}`;
+  }
+
+  private openMap() {
     this.modalCtrl
         .create({ component: MapModalComponent })
         .then(modalEl => {
@@ -49,30 +88,36 @@ export class LocationPickerComponent implements OnInit {
                       return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
                     })
                 ).subscribe(staticMapImageUrl => {
-                  pickedLocation.staticMapImageUrl = staticMapImageUrl;
-                  this.selectedLocationImage = staticMapImageUrl;
-                  this.isLoading = false;
-                  this.locationPick.emit(pickedLocation);
+              pickedLocation.staticMapImageUrl = staticMapImageUrl;
+              this.selectedLocationImage = staticMapImageUrl;
+              this.isLoading = false;
+              this.locationPick.emit(pickedLocation);
             });
           });
           modalEl.present();
         });
   }
 
-  private getAddress(lat: number, lng: number) {
-    return this.http
-        .get<any>(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${this.GOOGLE_API_KEY}`)
-        .pipe(
-            map((geoData: any) => {
-              if (!geoData || !geoData.results || geoData.results.length === 0) {
-                return null;
-              }
-              return geoData.results[0].formatted_address;
-            })
-        );
+  private locateUser() {
+    if (!Capacitor.isPluginAvailable('Geolocation')) {
+      this.showErrorAlert();
+      return;
+    }
+
+    Plugins.Geolocation
+        .getCurrentPosition()
+        .then(geoPosition => {
+          const coordinates: Coordinates = {lat: geoPosition.coords.latitude, lng: geoPosition.coords.longitude};
+        })
+        .catch(err => this.showErrorAlert());
   }
 
-  private getMapImage(lat: number, lng: number, zoom: number) {
-    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x300&maptype=roadmap&markers=color:red%7Clabel:Place%7C${lat},${lng}&key=${this.GOOGLE_API_KEY}`;
+  private showErrorAlert() {
+    this.alertCtrl.create(
+        {
+          header: 'Could not fetch location',
+          message: 'Please use the map to pick a location!'
+        }
+    ).then(alertEl => alertEl.present());
   }
 }
