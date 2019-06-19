14db49048a913b79f19337506fcda661cf41f4a2
Marco Gordillo <6589245+marcopgordillo@users.noreply.github.com>
2019-06-19 10:35:57 -0400

    Adding autologin

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, from} from 'rxjs';
 import {User} from './user.model';
 import {map, tap} from 'rxjs/operators';
 import {Plugins} from '@capacitor/core';
@@ -77,14 +77,39 @@ export class AuthService {
         );
   }
 
+  autoLogin() {
+    return from(Plugins.Storage.get({key: 'authData'}))
+        .pipe(
+            map(storedData => {
+              if (!storedData || !storedData.value) {
+                return null;
+              }
+              const parsedData = JSON.parse(storedData.value) as {token: string, tokenExpirationDate: string, userId: string, email: string};
+              const expirationTime = new Date(parsedData.tokenExpirationDate);
+              if (expirationTime <= new Date()) {
+                return null;
+              }
+              return new User(parsedData.userId, parsedData.email, parsedData.token, expirationTime);
+            }),
+            tap(user => {
+              if (user) {
+                this._user.next(user);
+              }
+            }),
+            map(user => {
+              return !!user;
+            })
+        ); // convert to observable
+  }
+
   private setUserData(userData: AuthResponseData) {
     const expirationTime = new Date(new Date().getTime() + (+userData.expiresIn * 1000));
     this._user.next(new User(userData.localId, userData.email, userData.idToken, expirationTime ));
-    this.storageAuthData(userData.localId, userData.idToken, expirationTime.toISOString());
+    this.storageAuthData(userData.localId, userData.idToken, expirationTime.toISOString(), userData.email);
   }
 
-  private storageAuthData(userId: string, token: string, tokenExpirationDate: string) {
-    const data = JSON.stringify({userId, token, tokenExpirationDate});
+  private storageAuthData(userId: string, token: string, tokenExpirationDate: string, email: string) {
+    const data = JSON.stringify({userId, token, tokenExpirationDate, email});
     Plugins.Storage.set({ key: 'authData', value: data });
   }
 }
